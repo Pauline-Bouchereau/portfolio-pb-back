9c6d8924f84df10649de12ea3d742725cbef5793
Pauline-Bouchereau <pauline-bouchereau@gmail.com>
2021-07-14 23:37:40 +0200

    project model

---
+++ models/Project.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Project = mongoose.model("Project", {
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    // Frontend or fullstack
+    type: String,
+    required: true,
+  },
+  techno: {
+    type: Array,
+    required: true,
+  },
+  doneAt: {
+    // Personnal, Reacteur, Pro
+    type: String,
+    default: "",
+  },
+  groupProject: {
+    type: Boolean,
+  },
+  frontRepo: {
+    type: String,
+    default: "",
+  },
+  backRepo: {
+    type: String,
+    default: "",
+  },
+  liveSite: {
+    type: String,
+    default: "",
+  },
+  device: {
+    // desktop or mobile
+    type: String,
+  },
+  image: { type: mongoose.Schema.Types.Mixed, default: {} },
+  video: { type: mongoose.Schema.Types.Mixed, default: {} },
+});
