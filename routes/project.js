cc8c4629e10d7749171bb07d760a84e3b84ccb69
Pauline-Bouchereau <pauline-bouchereau@gmail.com>
2021-07-18 22:09:52 +0200

    route to get all projects

--- routes/project.js
+++ routes/project.js
@@ -100,7 +100,7 @@ router.post("/project/create", isAuthenticated, async (req, res) => {
 });
 
 // Route to delete a project from BDD
-router.delete("/project/delete", isAuthenticated, async (req, res) => {
+router.delete("/project/delete/:id", isAuthenticated, async (req, res) => {
   try {
     // Check if ID in params
     if (req.params.id) {
@@ -139,10 +139,143 @@ router.delete("/project/delete", isAuthenticated, async (req, res) => {
 });
 
 // Route to modify a project from BDD
+router.put("/project/update/:id", async (req, res) => {
+  try {
+    // Check if id sent in params
+    if (req.params.id) {
+      // Check if project id exist in DB
+      const projectToUpdate = await Project.findById(req.params.id);
+
+      if (projectToUpdate) {
+        const {
+          name,
+          type,
+          techno,
+          doneAt,
+          groupProject,
+          frontRepo,
+          backRepo,
+          liveSite,
+          device,
+        } = req.fields;
+
+        const { image, video } = req.files;
+
+        if (name) {
+          projectToUpdate.name = name;
+        }
+
+        if (type) {
+          projectToUpdate.type = type;
+        }
+
+        if (techno) {
+          projectToUpdate.techno = techno;
+        }
+
+        if (doneAt) {
+          projectToUpdate.doneAt = doneAt;
+        }
+
+        if (groupProject) {
+          projectToUpdate.groupProject = groupProject;
+        }
+
+        if (frontRepo) {
+          projectToUpdate.frontRepo = frontRepo;
+        }
+
+        if (backRepo) {
+          projectToUpdate.backRepo = backRepo;
+        }
+
+        if (liveSite) {
+          projectToUpdate.liveSite = liveSite;
+        }
+
+        if (device) {
+          projectToUpdate.device = device;
+        }
+
+        if (image) {
+          if (projectToUpdate.image) {
+            // Delete previous image from Cloudinary
+            await cloudinary.api.delete_resources_by_prefix(
+              `/portfolio/projectsIMG/${projectToUpdate.name}`
+            );
+          }
+
+          const imageProject = image.path;
+
+          // Upload image to Cloudinary
+          let result = await cloudinary.uploader.upload(imageProject, {
+            folder: `/portfolio/projectsIMG/${projectToUpdate.name}`,
+          });
+
+          newProject.image = result;
+        }
+
+        if (video) {
+          if (projectToUpdate.video) {
+            // Delete previous image from Cloudinary
+            await cloudinary.api.delete_resources_by_prefix(
+              `/portfolio/projectsVID/${projectToUpdate.name}`
+            );
+          }
+
+          const videoProject = video.path;
+
+          // Upload video to Cloudinary
+          let result = await cloudinary.uploader.upload(videoProject, {
+            folder: `/portfolio/projectsVID/${projectToUpdate.name}`,
+          });
+
+          newProject.video = result;
+        }
+
+        // Save updated project in DB
+        await projectToUpdate.save();
+
+        // Respond to client
+        res.status(200).json({ message: "Project successfully updated." });
+      }
+    } else {
+      res.status(400).json({ error: "Missing project ID" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 // Route to get all projects (with filters)
 router.get("/projects", async (req, res) => {
   try {
+    const { type, techno, doneAt, device } = req.query;
+
+    // Check if filters sent in query, if yes, add it to the object filters
+    const filters = {};
+
+    if (type) {
+      filters.type = type;
+    }
+
+    if (techno) {
+      filters.techno = techno;
+    }
+
+    if (doneAt) {
+      filters.doneAt = doneAt;
+    }
+
+    if (device) {
+      filters.device = device;
+    }
+
+    // Search projects in DB with filters
+    const result = await Project.find(filters);
+
+    // Respond to client
+    res.status(200).json(result);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
